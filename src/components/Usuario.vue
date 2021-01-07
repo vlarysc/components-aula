92ba047600e2b53f74f20afdb861b71fa4f0adb4
Vlary <manoelfranciscose@gmail.com>
2021-01-07 16:52:20 -0300

    components

---
+++ src/components/Usuario.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container">
+        <h1>Componente Usuário</h1>
+        <p>Esse é um componente muito legal!</p>
+        <button @click="alterarNome">Alterar Nome</button>
+        <hr>
+        <div class="componentes">
+            <app-usuario-info :nome="nome" />
+            <app-usuario-editar  />
+        </div>
+    </div>
+</template>
+
+<script>
+import AppUsuarioInfo from './UsuarioInfo'
+import AppUsuarioEditar from './UsuarioEditar'
+
+export default {
+    components: { AppUsuarioInfo, AppUsuarioEditar },
+    data() {
+        return {
+            nome: 'Pedro'
+        }
+    },
+    methods: {
+        alterarNome() {
+            this.nome = 'Maria'
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .container {
+        background-color: #27363b;
+        color: #fff;
+        padding: 10px;
+    }
+
+    .container hr {
+        margin: 20px 10px;
+    }
+
+    .componentes {
+        display: flex;
+    }
+
+    .componentes > * {
+        margin: 10px;
+    }
+</style>
---
+++ src/components/UsuarioInfo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome de usuário: <strong>{{ inverterNome() }}</strong> </p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['nome'],
+    methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('')
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
