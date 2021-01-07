3c730e011babf014457b38e64866459203da26ca
Vlary <manoelfranciscose@gmail.com>
2021-01-07 17:03:50 -0300

    aula

--- src/components/UsuarioInfo.vue
+++ src/components/UsuarioInfo.vue
@@ -2,18 +2,14 @@
     <div class="componente">
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
-        <p>Nome de usuário: <strong>{{ inverterNome() }}</strong> </p>
+        <p>Nome de usuário: <strong>{{ nome }}</strong> </p>
     </div>
 </template>
 
 <script>
 export default {
     props: ['nome'],
-    methods: {
-        inverterNome() {
-            return this.nome.split('').reverse().join('')
-        }
-    }
+    
     
 }
 </script>
