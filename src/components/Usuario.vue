d1c9fc575367e866966265e14e4b6cc20dc12d2c
Vlary <manoelfranciscose@gmail.com>
2021-01-08 08:26:33 -0300

    aula component

--- src/components/Usuario.vue
+++ src/components/Usuario.vue
@@ -2,10 +2,13 @@
     <div class="container">
         <h1>Componente Usuário</h1>
         <p>Esse é um componente muito legal!</p>
+        <p>Nome é <strong>{{ nome }}</strong></p>
         <button @click="alterarNome">Alterar Nome</button>
         <hr>
         <div class="componentes">
-            <app-usuario-info :nome="nome" />
+            <app-usuario-info 
+            :nome="nome"
+            @nomeMudou="nome = $event" />
             <app-usuario-editar  />
         </div>
     </div>
@@ -25,7 +28,8 @@ export default {
     methods: {
         alterarNome() {
             this.nome = 'Maria'
-        }
+        },
+        
     }
 }
 </script>
--- src/components/UsuarioInfo.vue
+++ src/components/UsuarioInfo.vue
@@ -2,13 +2,23 @@
     <div class="componente">
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
+        
         <p>Nome de usuário: <strong>{{ nome }}</strong> </p>
+        <button @click="reniciarNome">Reniciar Nome</button>
+
     </div>
 </template>
 
 <script>
 export default {
     props: ['nome'],
+    methods: {
+        reniciarNome() {
+            this.nome = 'Pedro'
+            this.$emit('nomeMudou', this.nome)
+        }
+
+    }
     
     
 }
